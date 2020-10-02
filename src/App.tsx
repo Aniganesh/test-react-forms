8c42e2d7fe2a761f49b90da5064eefdab3180c97
aniganesh <aniganesh741@gmail.com>
2020-10-02 23:36:56 +0530

    Fresh testing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,36 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-// import _ from 'lodash'
-import { attachField, FormConfig, IFormActionProps, TFile, ReactForm } from 'react-forms'
-// import { MUIRating } from './MUIRating'
-import { IconButton } from '@material-ui/core';
-import { Add } from '@material-ui/icons';
-import 'rf-dropzone-file-picker'
-import 'rf-ratings'
+import { IFormActionProps, ReactForm } from 'react-forms'
+import { IconButton } from '@material-ui/core'
+import { Add } from '@material-ui/icons'
 
-const handleChange = (files: TFile[], remFiles?: TFile[]) => {
-  console.log("Fn passed to onDone", files.concat(remFiles ? remFiles : []))
-}
-const onDrop = (files: any) => {
-  console.log(files)
-}
-
-// attachField("rating", <MUIRating />)
-const CONFIG: Array<Array<FormConfig> | FormConfig> = [
+const CONFIG = [
+  { type: 'text', valueKey: 'text' },
   {
-    type: 'text',
-    valueKey: 'Email',
-  },
-  {
-    type: 'dropFile',
-    valueKey: 'files',
-    fieldProps: {
-      multiple: true,
-      onDropFile: onDrop,
-    }
-  }, {
-    type: 'file',
-    valueKey: 'files2',
-    fieldProps: {
-      multiple: true,
-      onDone: handleChange,
+    type: 'file', valueKey: 'file', fieldProps: {
       wrapWith: (input: JSX.Element) => <IconButton><Add />{input}</IconButton>,
+      multiple: true,
     }
   },
-  {
-    type: 'rating',
-    valueKey: 'rating',
-    fieldProps: {
-    }
-  }
+  { type: '', valueKey: '' }
 ]
+const actionConfig: IFormActionProps = {
+  submitButtonText: 'Submit',
+  submitButtonProps: {
+    size: 'large'
+  },
+}
 const handleSubmit = (formData: any) => {
   console.log(formData)
 }
 function App() {
-
-  const actionConfig: IFormActionProps = {
-    submitButtonText: 'Submit',
-    submitButtonProps: {
-      size: 'large'
-    },
-  };
   return (
     <div className="App">
-      <ReactForm formId="login-form"
-        config={CONFIG}
-        formSettings={{
-          verticalSpacing: 20
-        }}
-        onSubmit={handleSubmit}
-        actionConfig={actionConfig}
-      >
-      </ReactForm>
-      {/* <MUIDropFile fieldProps={{multiple:true, onDropFile:onDrop}} />
-      <MUIFileInput fieldProps={{ invisible: true, multiple: true, onDone: handleChange, WrapWith: (input: JSX.Element) => <IconButton><Add />{input}</IconButton>, }} /> */}
-      {/* <MUIRating /> */}
+      <div className='App-header'>
+        <ReactForm config={CONFIG} formId="test-form" actionConfig={actionConfig}
+          onSubmit={handleSubmit} />
+      </div>
     </div>
   );
 }
